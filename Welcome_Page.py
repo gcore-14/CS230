3827196303bad251a13fb438104ba0e4171a37bc
gcore-14 <132623768+gcore-14@users.noreply.github.com>
2023-05-04 19:08:31 -0400

    Update Welcome_Page.py

--- Welcome_Page.py
+++ Welcome_Page.py
@@ -18,7 +18,7 @@ st.title("Welcome to the World of Meteors!")
 
 st.sidebar.success("Select a demo above.")
 
-image = Image.open('In class/final/190801-meteor-shower-cs-124p.jpg')
+image = Image.open('190801-meteor-shower-cs-124p.jpg')
 st.image(image)
 
 
