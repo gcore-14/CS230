825b9f71aec8cff3d113b7f3ce152185e4624b9d
gcore-14 <132623768+gcore-14@users.noreply.github.com>
2023-05-04 20:36:43 -0400

    Update Welcome_Page.py

--- Welcome_Page.py
+++ Welcome_Page.py
@@ -4,7 +4,8 @@ CS230: Section 3
 Data: Meteorite Landings
 Description:
 This program takes in 2 csv files, turning them both into dataframes, then using those dataframes for images such as maps, pie-charts, bar graphs, etc.)
-Extra Credit: used Lambda in ______ and uploaded to Streamlit.io (https://gcore-14-cs230-welcome-page-3d9nyj.streamlit.app/)
+Extra Credit: used Lambda in meteor_landings_per_year.py to sort the data into only positive years and years < 2023, as some years were 
+above it and some where no year was given and uploaded to Streamlit.io (https://gcore-14-cs230-welcome-page-3d9nyj.streamlit.app/)
 """
 
 import streamlit as st
