9e95d07c76c116f8155beb4161e74e525b6e5309
gcore-14 <132623768+gcore-14@users.noreply.github.com>
2023-05-04 21:40:25 -0400

    Update Meteor_Landings_Per_Year.py

--- pages/Meteor_Landings_Per_Year.py
+++ pages/Meteor_Landings_Per_Year.py
@@ -9,13 +9,17 @@ dfinfo = dfinfo[dfinfo['year'] <= 2023]
 
 def yearfreq(years):
    year_counts = {}
+   #using lambda to get rid of years less than 0 greater than the current year
    dataframe = years['year'].apply(lambda x: None if x < 0 or x > 2024 else x)
-
+#using similar count method from selecting classifications to do the count
+#if the year_counts is in both dataframe and year_counts then it adds by 1, if not it adds it to the dictionary
+#i shows the years
    for i in dataframe:
        if i in year_counts:
            year_counts[i] += 1
        else:
            year_counts[i] = 1
+   #getting the min and max year to show on the slider
    min_year = int(dataframe.min())
    max_year = int(dataframe.max())
    start, end = st.slider(
@@ -25,8 +29,10 @@ def yearfreq(years):
    st.write(f'Outputted range will be in between {start} and {end}')
 
 
-
+#adding another function so if the button is pressed then the above code is run
    def run_code():
+      #list comprehension so the dates and totals are computed
+      #if the the year is in between start and end then dates = the given year, the same for totals, but it is the total amount per year
        dates = [year[0] for year in year_counts.items() if start <= year[0] <= end]
        totals = [year[1] for year in year_counts.items() if start <= year[0] <= end]
        data = {'year': dates, 'count': totals}
