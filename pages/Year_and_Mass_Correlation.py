8bf0b046a07719bfce84dfd018ef5df34bf736a3
gcore-14 <132623768+gcore-14@users.noreply.github.com>
2023-05-04 21:46:23 -0400

    Update Year_and_Mass_Correlation.py

--- pages/Year_and_Mass_Correlation.py
+++ pages/Year_and_Mass_Correlation.py
@@ -5,10 +5,11 @@ import matplotlib.pyplot as plt
 dfinfo = pd.read_csv("Meteorite_Landings.csv")
 dfinfo = dfinfo.dropna()
 dfinfo = dfinfo[dfinfo['year'] <= 2023]
-
+#making sure the year is less than 2023
 st.subheader('Year and Mass Correlation')
 st.write('Choose what year to start with!')
 def scatter(plot):
+    #asking the user for a number to update the scatter plot using year and mass (g) then plotting it
     number = st.number_input(f"Type in a number in between {int(plot['year'].min())} and {int(plot['year'].max())}")
     plot = plot[plot['year'] >= number]
     fig, ax = plt.subplots()
