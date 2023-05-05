99dda78828470b2c9aa8867ba821482ec299f6cf
gcore-14 <132623768+gcore-14@users.noreply.github.com>
2023-05-04 21:32:26 -0400

    Update Filter_or_Sort_the_Dataframe_to_your_liking.py

--- pages/Filter_or_Sort_the_Dataframe_to_your_liking.py
+++ pages/Filter_or_Sort_the_Dataframe_to_your_liking.py
@@ -29,15 +29,15 @@ def filter_dataframe(df: pd.DataFrame) -> pd.DataFrame:
 
     
 
-        if is_datetime64_any_dtype(df[col]):
-            df[col] = df[col].dt.tz_localize(None)
 
     modification_container = st.container()
 
     with modification_container:
+        #creating a drop down filter, using the columns as the options
         to_filter_columns = st.multiselect("Filter dataframe on", df.columns)
         for column in to_filter_columns:
             left, right = st.columns((1, 20))
+            #a drop down if the filter has more options
             left.write("↳")
             # Treat columns with < 10 unique values as categorical
             if is_categorical_dtype(df[column]) or df[column].nunique() < 10:
@@ -46,7 +46,9 @@ def filter_dataframe(df: pd.DataFrame) -> pd.DataFrame:
                     df[column].unique(),
                     default=list(df[column].unique()),
                 )
+                #filtering the dataframe based on the user choice
                 df = df[df[column].isin(user_cat_input)]
+            #if the column has numbers, display a slider
             elif is_numeric_dtype(df[column]):
                 _min = float(df[column].min())
                 _max = float(df[column].max())
@@ -59,6 +61,7 @@ def filter_dataframe(df: pd.DataFrame) -> pd.DataFrame:
                     step=step,
                 )
                 df = df[df[column].between(*user_num_input)]
+                #creating the inputs to be a min and max of the columns if there is a date
             elif is_datetime64_any_dtype(df[column]):
                 user_date_input = right.date_input(
                     f"Values for {column}",
@@ -71,6 +74,7 @@ def filter_dataframe(df: pd.DataFrame) -> pd.DataFrame:
                     user_date_input = tuple(map(pd.to_datetime, user_date_input))
                     start_date, end_date = user_date_input
                     df = df.loc[df[column].between(start_date, end_date)]
+            #if the column option isn't any of the above it creates a space for the user to input text of the specifics they want to sort or filter by
             else:
                 user_text_input = right.text_input(
                     f"Substring or regex in {column}",
