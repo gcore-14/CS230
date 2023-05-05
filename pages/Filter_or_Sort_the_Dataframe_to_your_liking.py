ecef9f86422bd3916dfdb973deaa59d98d2fafe7
gcore-14 <132623768+gcore-14@users.noreply.github.com>
2023-05-04 21:24:28 -0400

    Update Filter_or_Sort_the_Dataframe_to_your_liking.py

--- pages/Filter_or_Sort_the_Dataframe_to_your_liking.py
+++ pages/Filter_or_Sort_the_Dataframe_to_your_liking.py
@@ -22,19 +22,12 @@ def filter_dataframe(df: pd.DataFrame) -> pd.DataFrame:
     from https://github.com/tylerjrichards/st-filter-dataframe/blob/main/streamlit_app.py
     """
     modify = st.checkbox("Add filters")
+    #creating a checkbox so users can add filters 
 
     if not modify:
         return df
 
-    df = df.copy()
-
-    # Try to convert datetimes into a standard format (datetime, no timezone)
-    for col in df.columns:
-        if is_object_dtype(df[col]):
-            try:
-                df[col] = pd.to_datetime(df[col])
-            except Exception:
-                pass
+    
 
         if is_datetime64_any_dtype(df[col]):
             df[col] = df[col].dt.tz_localize(None)
